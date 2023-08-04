aae74a20f56646cfb3633d0f971cf7705c2b77d3
DaviZCodes <davi.bestguy@gmail.com>
2023-08-04 19:16:09 -0400

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 py-4 text-white text-center">
-      <p>&copy; {new Date().getFullYear()} Davi Zheng. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Davi Zheng. Reach me.</p>
     </footer>
   );
 }
