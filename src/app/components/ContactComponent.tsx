aecbb14d14a46267acbda69a040cf88ed9ba64d0
DaviZCodes <davi.bestguy@gmail.com>
2023-08-04 18:41:11 -0400

    website home page

---
+++ src/app/components/ContactComponent.tsx
@@ -0,0 +1,14 @@
+export default function ContactSection() {
+  return (
+    <section className="bg-blue-800 py-12">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-bold text-white mb-4">Let's Connect</h2>
+        <p className="text-xl text-white">
+          I'm open to collaborating on projects, whether it is coding,
+          designing, video-making or anythings. Feel free to reach out to me
+          through any of the links below:
+        </p>
+      </div>
+    </section>
+  );
+}
