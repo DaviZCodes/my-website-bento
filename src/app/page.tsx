aa128dada70765b87eb5b39f660b0056adb95087
DaviZCodes <davi.bestguy@gmail.com>
2023-08-05 17:00:16 -0400

    fixed links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,60 +35,62 @@ const Home = () => {
           <h2 className="text-3xl font-bold mb-4">Work Experience</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="bg-gray-200 p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
-              <h3 className="text-xl font-bold mb-2">
-                Software Engineer Intern
-              </h3>
-              <h2 className="text-xl cursor-pointer text-yellow-600 font-semibold">
-                <Link
-                  href="https://www.paysfer.com"
-                  target="_blank"
-                  title="Click for more"
-                >
+              <Link
+                href="https://www.paysfer.com"
+                target="_blank"
+                title="Click for more"
+              >
+                <h3 className="text-xl font-bold mb-2">
+                  Software Engineer Intern
+                </h3>
+                <h2 className="text-xl cursor-pointer text-yellow-600 font-semibold">
                   Paysfer eMart
-                </Link>
-              </h2>
-              <p>
-                I was a full-stack engineer, developing several responsive
-                webpages, enhancing the user authentication and payment
-                gateways, and creating APIs that fetch user data and shops.
-              </p>
+                </h2>
+                <p>
+                  I was a full-stack engineer, developing several responsive
+                  webpages, enhancing the user authentication and payment
+                  gateways, and creating APIs that fetch user data and shops.
+                </p>
+              </Link>
             </div>
 
             <div className="bg-gray-200 p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
-              <h3 className="text-xl font-bold mb-2">Computer Science Tutor</h3>
-              <h2 className="text-xl cursor-pointer text-purple-800 font-semibold">
-                <Link
-                  href="https://engineering.nyu.edu/academics/undergraduate/tutoring-center"
-                  target="_blank"
-                  title="Click for more"
-                >
+              <Link
+                href="https://engineering.nyu.edu/academics/undergraduate/tutoring-center"
+                target="_blank"
+                title="Click for more"
+              >
+                <h3 className="text-xl font-bold mb-2">
+                  Computer Science Tutor
+                </h3>
+                <h2 className="text-xl cursor-pointer text-purple-800 font-semibold">
                   Polytechnic Tutoring Center
-                </Link>
-              </h2>
-              <p>
-                I was a tutor for the Introduction to Programming, Data
-                Structures and Algorithms, and Object-Oriented Programming
-                courses at NYU.
-              </p>
+                </h2>
+                <p>
+                  I was a tutor for the Introduction to Programming, Data
+                  Structures and Algorithms, and Object-Oriented Programming
+                  courses at NYU.
+                </p>
+              </Link>
             </div>
 
             <div className="bg-gray-200 p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
-              <h3 className="text-xl font-bold mb-2">
-                Software Engineer Intern
-              </h3>
-              <h2 className="text-xl cursor-pointe text-purple-500 font-semibold">
-                <Link
-                  href="https://www.tutormundi.com"
-                  target="_blank"
-                  title="Click for more"
-                >
+              <Link
+                href="https://www.tutormundi.com"
+                target="_blank"
+                title="Click for more"
+              >
+                <h3 className="text-xl font-bold mb-2">
+                  Software Engineer Intern
+                </h3>
+                <h2 className="text-xl cursor-pointe text-purple-500 font-semibold">
                   TutorMundi
-                </Link>
-              </h2>
-              <p>
-                I was a frontend developer, identifying and resolving numerous
-                bugs, while also making improvements to the user interface.
-              </p>
+                </h2>
+                <p>
+                  I was a frontend developer, identifying and resolving numerous
+                  bugs, while also making improvements to the user interface.
+                </p>
+              </Link>
             </div>
           </div>
         </div>
