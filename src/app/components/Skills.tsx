4a58e7b721e713f09d4fe026af17cbef47bbf766
DaviZCodes <davi.bestguy@gmail.com>
2023-08-08 16:30:35 -0400

    skills updated

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -4,20 +4,21 @@ export default function Skills() {
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-4">Skills and frameworks</h2>
         <ul className="list-disc pl-3">
+          <li>C, C++, x86 Assembly</li>
           <li>
             Python, OpenCV, AprilTag, ROS, Machine Learning, Convolutional
             Neural Networks
           </li>
-          <li>C, C++, x86 Assembly</li>
+
           <li>
-            JavaScript, TypeScript, React, NextJS, Tailwind, SCSS, NodeJS,
-            Express, Convex.dev, Postman, SQL, MongoDB
+            HTML, CSS, JavaScript, TypeScript, React, NextJS, Tailwind, SCSS,
+            NodeJS, Express, Convex.dev, Postman, SQL, MongoDB
           </li>
           <li>
             Video Editing (Final Cut Pro, Sony Vegas Pro, Davinci Resolve),
             Adobe Photoshop, Figma
           </li>
-          <li>Google Suite, Microsoft Office</li>
+          <li>Google Suite, Microsoft Office, macOS, Windows, Linux</li>
         </ul>
         <h2 className="text-3xl font-bold mt-6 mb-4">Hobbies</h2>
         <ul className="list-disc pl-3">
