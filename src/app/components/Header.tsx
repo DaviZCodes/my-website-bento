cd90165c1fa1c28fca50baa7024c0d2109509f37
DaviZCodes <davi.bestguy@gmail.com>
2023-08-10 19:35:38 -0400

    cat lover

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,9 +4,7 @@ export default function Header() {
       <div className="text-white text-center py-8 p-2">
         <h1 className="text-3xl font-bold pb-3">Davi Zheng</h1>
         <div className="text-xl">Full-Stack Engineer</div>
-        <div className="text-xl">
-          Tesla Enjoyer, Team Player, and AI Enthusiast
-        </div>
+        <div className="text-xl">Cat Lover, Team Player, and AI Enthusiast</div>
         <div className="text-xl">Computer Science Student @ NYU</div>
       </div>
     </header>
