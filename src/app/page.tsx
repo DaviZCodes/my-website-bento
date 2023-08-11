d7183065fa23df4c579f5f9f64bbba48cfa51c5d
DaviZCodes <davi.bestguy@gmail.com>
2023-08-11 12:21:15 -0400

    color accessbilities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ const Home = () => {
         <div className="container mx-auto">
           <h2 className="text-3xl font-bold mb-4">Work Experience</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div className="bg-gray-200 p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
+            <div className="bg-[#d6a511] p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
               <Link
                 href="https://www.paysfer.com"
                 target="_blank"
@@ -32,7 +32,7 @@ const Home = () => {
                 <h3 className="text-xl font-bold mb-2">
                   Software Engineer Intern
                 </h3>
-                <h2 className="text-xl cursor-pointer text-[#d6a511] font-semibold">
+                <h2 className="text-xl cursor-pointer font-semibold">
                   Paysfer eMart
                 </h2>
                 <p>
@@ -43,7 +43,7 @@ const Home = () => {
               </Link>
             </div>
 
-            <div className="bg-gray-200 p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
+            <div className="bg-[#4d1782] text-white p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
               <Link
                 href="https://engineering.nyu.edu/academics/undergraduate/tutoring-center"
                 target="_blank"
@@ -52,7 +52,7 @@ const Home = () => {
                 <h3 className="text-xl font-bold mb-2">
                   Computer Science Tutor
                 </h3>
-                <h2 className="text-xl cursor-pointer text-[#4d1782] font-semibold">
+                <h2 className="text-xl cursor-pointer font-semibold">
                   Polytechnic Tutoring Center
                 </h2>
                 <p>
@@ -63,7 +63,7 @@ const Home = () => {
               </Link>
             </div>
 
-            <div className="bg-gray-200 p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
+            <div className="bg-[#5c25f5] text-white p-6 rounded-lg shadow-md transition-transform ease-in-out duration-300 scale-x-100 hover:scale-[1.02] cursor-pointer">
               <Link
                 href="https://www.tutormundi.com"
                 target="_blank"
@@ -72,7 +72,7 @@ const Home = () => {
                 <h3 className="text-xl font-bold mb-2">
                   Software Engineer Intern
                 </h3>
-                <h2 className="text-xl cursor-pointe text-[#5c25f5] font-semibold">
+                <h2 className="text-xl cursor-pointe font-semibold">
                   TutorMundi
                 </h2>
                 <p>
