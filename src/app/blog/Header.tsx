391a7ad7cddfa96638d418efd8457efc19225783
DaviZCodes <davi.bestguy@gmail.com>
2023-10-19 00:30:56 -0400

    fixed home href

--- src/app/blog/Header.tsx
+++ src/app/blog/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         </h1>
       </div>
       <div className="flex justify-center absolute top-0 w-full pt-3 sm:w-auto sm:pl-2 sm:pt-2">
-        <Link href="/blog" className="text-white font-semibold text-lg bold">
+        <Link href="/" className="text-white font-semibold text-lg bold">
           Home
         </Link>
       </div>
