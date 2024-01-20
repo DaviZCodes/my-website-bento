33f35422126e0ebfc9117ced4a81ca1c3995adad
DaviZCodes <davi.bestguy@gmail.com>
2024-01-20 18:45:58 -0500

    updated blog and added images for hobbies

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import chessKnight from "./images/chess piece.webp";
+import basketball from "./images/basketball.png";
+import dumbbell from "./images/dumbbell.webp";
+
 export default function Skills() {
   return (
     <section className="bg-white py-12 px-2 lg:px-3">
@@ -21,18 +26,58 @@ export default function Skills() {
           <li>Google Suite, Microsoft Office, macOS, Windows, Linux</li>
         </ul>
         <h2 className="text-3xl font-bold mt-6 mb-4">Hobbies</h2>
-        <ul className="list-disc pl-3">
-          <li>
-            I love playing chess. I just started playing two years ago and
-            I&apos;m rated around 1800 on chess.com. Want to play?
-          </li>
-          <li>
-            I actively play sports and work out. I have played varsity
-            basketball, varsity football, and badminton at a state level. Would
-            you like to work out together? (My goal is to bench 225 pounds and
-            get a six pack)
-          </li>
-        </ul>
+        <div className="flex justify-center items-center">
+          <div>
+            <div className="w-28 lg:w-36">
+              <Image
+                src={chessKnight}
+                className="w-full h-full mb-2"
+                width={0}
+                height={0}
+                sizes="100vm"
+                alt="Chess Piece"
+                loading="lazy"
+              />
+            </div>
+            <p>
+              I love playing chess. I just started playing two years ago and
+              I&apos;m rated around 1800 on chess.com. Want to play?
+            </p>
+          </div>
+
+          <div>
+            <div className="flex items-center">
+              <div className="w-60">
+                <Image
+                  src={basketball}
+                  className="w-full h-full"
+                  width={0}
+                  height={0}
+                  sizes="100vm"
+                  alt="Basketball"
+                  loading="lazy"
+                />
+              </div>
+              <div className="h-32">
+                <Image
+                  src={dumbbell}
+                  className="w-full h-full"
+                  width={0}
+                  height={0}
+                  sizes="100vm"
+                  alt="Basketball"
+                  loading="lazy"
+                />
+              </div>
+            </div>
+            <p>
+              I actively play sports and work out. I have played varsity
+              basketball, varsity football, and badminton at a state level.
+              Would you like to work out together? (My goal is to bench 225
+              pounds and get a six pack)
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
