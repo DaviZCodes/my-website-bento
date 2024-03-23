460ea388a7f86a75201b5c83e1d5854606439396
DaviZCodes <davi.bestguy@gmail.com>
2024-03-23 14:43:24 -0400

    added LinkedIn link

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function AboutMe() {
   return (
     <section className="py-12 px-3 lg:px-5">
@@ -6,14 +8,21 @@ export default function AboutMe() {
 
         <p className="text-lg">
           I figured this is my personal website, so I will make it personal. If
-          you want formality, visit my LinkedIn instead. This might seem false
-          but I actually enjoy writing code. I love showing my family and
-          friends my applications. I think that&apos;s the best thing about
-          studying CS. Also, working in teams is fun. Being humble and striving
-          to improve my skills is fun. Python and JavaScript are awesome
-          languages. Data Structures and Algorithms is not fun. But I&apos;m
-          still reading Cracking the Coding Interview because I have to.
-          Hopefully, one day it works out.
+          you want formality, visit my{" "}
+          <Link
+            href="https://www.linkedin.com/in/davizheng/"
+            target="_blank"
+            title="Click for LinkedIn"
+          >
+            LinkedIn
+          </Link>{" "}
+          instead. This might seem false but I actually enjoy writing code. I
+          love showing my family and friends my applications. I think
+          that&apos;s the best thing about studying CS. Also, working in teams
+          is fun. Being humble and striving to improve my skills is fun. Python
+          and JavaScript are awesome languages. Data Structures and Algorithms
+          is not fun. But I&apos;m still reading Cracking the Coding Interview
+          because I have to. Hopefully, one day it works out.
           {/*Hello, I&apos;m Davi, a multifaceted full-stack engineer passionate
           about creating modern and responsive applications. I&apos;m fluent in
           four programming languages - Python, C, C++, and
