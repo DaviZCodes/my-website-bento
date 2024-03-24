20a5c2010dce2009de9ec1f00f7a29a85d9e31fe
DaviZCodes <davi.bestguy@gmail.com>
2024-03-23 22:31:20 -0400

    NavBar is looking good

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,9 +2,9 @@ import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <header className="bg-blue-800 pt-20 pb-16">
-      <div className="text-white text-center py-6 p-10">
-        <h1 className="text-6xl font-bold pt-5 sm:pt-0 pb-3">
+    <header className="bg-blue-800 pt-10 xl:pt-16 xl:pb-16">
+      <div className="text-white text-center py-3">
+        <h1 className="text-3xl xl:text-6xl font-bold pt-5 sm:pt-0 pb-3">
           Hello, I&apos;m Davi Zheng.
         </h1>
         <div className="text-xl">computer science nerd</div>
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,8 +1,39 @@
+"use client";
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
+  const [stickToTop, setStickToTop] = useState(false); // to keep NavBar stick after scroll
+
+  useEffect(() => {
+    // see if scrolled enough for NavBar to stick
+    const handleScroll = () => {
+      if (window.scrollY > 26) {
+        console.log("scroll above");
+        setStickToTop(true);
+      } else {
+        setStickToTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="absolute top-0 w-full py-6 px-12 flex justify-between items-center text-2xl">
+    <div
+      className={`top-0 w-full py-6 xl:px-28 flex justify-between items-center text-xl
+      ${
+        stickToTop
+          ? "fixed bg-blue-800 px-3 py-2 z-50 transition-all duration-300"
+          : "absolute w-full py-6 px-3 xl:px-12 flex justify-between items-center text-xl"
+      }`}
+    >
       <div>
         <Link href="/">
           <p className="text-white font-semibold text-2xl bold">Davi Zheng</p>
@@ -10,7 +41,7 @@ export default function NavBar() {
       </div>
 
       <div className="flex">
-        <div className="px-6">
+        <div className="">
           <Link href="/blog">
             <p className="text-white">Blog</p>
           </Link>
