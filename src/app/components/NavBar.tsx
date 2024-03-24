80a608842f0abdfac73197557792e62c9620538a
DaviZCodes <davi.bestguy@gmail.com>
2024-03-23 22:34:36 -0400

    added a border

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -30,7 +30,7 @@ export default function NavBar() {
       className={`top-0 w-full py-6 xl:px-28 flex justify-between items-center text-xl
       ${
         stickToTop
-          ? "fixed bg-blue-800 px-3 py-2 z-50 transition-all duration-300"
+          ? "fixed bg-blue-800 border px-3 py-2 z-50 transition-all duration-300"
           : "absolute w-full py-6 px-3 xl:px-12 flex justify-between items-center text-xl"
       }`}
     >
@@ -41,6 +41,16 @@ export default function NavBar() {
       </div>
 
       <div className="flex">
+        <div className="">
+          <Link href="/blog">
+            <p className="text-white">Work</p>
+          </Link>
+        </div>
+        <div className="px-6">
+          <Link href="/blog">
+            <p className="text-white">Projects</p>
+          </Link>
+        </div>
         <div className="">
           <Link href="/blog">
             <p className="text-white">Blog</p>
