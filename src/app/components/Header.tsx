8549948e63708dd4575edc2e41765b0924c3dcd4
DaviZCodes <davi.bestguy@gmail.com>
2024-03-28 22:38:53 -0400

    added words header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,12 +2,17 @@ import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <header className="bg-blue-800 pt-10 xl:pt-20 xl:pb-16">
+    <header className="bg-blue-800 pt-10 xl:pt-20 xl:pb-14">
       <div className="text-white text-center py-3">
         <h1 className="text-3xl xl:text-6xl font-bold pt-5 sm:pt-0 pb-3">
           Hello, I&apos;m Davi Zheng.
         </h1>
-        <div className="text-xl">computer science nerd</div>
+        <div className="text-xl">
+          Full-Stack Developer, Computer Science Nerd @ NYU
+        </div>
+        <div className="text-xl">
+          Optimist, Team Player, and Tech Enthusiast
+        </div>
       </div>
       <NavBar />
     </header>
