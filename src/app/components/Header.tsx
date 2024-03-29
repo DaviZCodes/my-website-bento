f81462cd55fb664ebfe8087a0ddf0d25cd6b7ef2
DaviZCodes <davi.bestguy@gmail.com>
2024-03-29 16:28:01 -0400

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="bg-blue-800 pt-10 xl:pt-20 xl:pb-14">
       <div className="text-white text-center py-3">
         <h1 className="text-3xl xl:text-6xl font-bold pt-5 sm:pt-0 pb-3">
-          Hello, I&apos;m Davi Zheng.
+          I&apos;m Davi Zheng.
         </h1>
         <div className="text-xl">
           Full-Stack Developer, Computer Science Nerd @ NYU
