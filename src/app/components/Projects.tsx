bad96092bc99fc1fe20dc315df589656e53b2d12
DaviZCodes <davi.bestguy@gmail.com>
2025-02-18 00:46:27 -0500

    need fix: projects section container

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
 import { HiExternalLink } from "react-icons/hi";
 
 export default function Projects() {
   return (
-    <section>
+    <section className="">
       <div className="flex flex-col border-2 border-yellow-50 transition duration-160 hover:border-[#fdaf67e3] rounded-xl shadow-sm p-6 bg-[#f5e2b6]">
         <div className="flex justify-between">
           <div className="font-bold text-2xl">Projects</div>
@@ -12,10 +14,51 @@ export default function Projects() {
           </button>
         </div>
 
-        <div className="flex justify-evenly text-xl">
-          <div>Arter</div>
-          <div>Lunar excavator</div>
-          <div>GitHub Licenses Ranked</div>
+        <div className="flex justify-between text-xl mt-3 gap-2">
+          <Link href="https://arter-generative-ai.vercel.app">
+            <div>Arter</div>
+            <div className="w-96">
+              <Image
+                src="https://i.imgur.com/l3q4T89.png"
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="w-full h-full"
+                title="Arter generative AI."
+                alt="Arter generative AI"
+              />
+            </div>
+          </Link>
+
+          <Link href="https://arter-generative-ai.vercel.app">
+            <div>Lunar excavator</div>
+            <div className="w-96">
+              <Image
+                src="https://i.imgur.com/l3q4T89.png"
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="w-full h-full"
+                title="Arter generative AI."
+                alt="Arter generative AI"
+              />
+            </div>
+          </Link>
+
+          <Link href="https://arter-generative-ai.vercel.app">
+            <div>GitHub Licenses Ranked</div>
+            <div className="w-96">
+              <Image
+                src="https://i.imgur.com/l3q4T89.png"
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="w-full h-full"
+                title="Arter generative AI."
+                alt="Arter generative AI"
+              />
+            </div>
+          </Link>
         </div>
       </div>
     </section>
